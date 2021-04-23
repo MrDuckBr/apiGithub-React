23a3a061437a91e81590e926df685211cd432302
MrDuckBr <wwalisson2012@gmail.com>
2021-04-23 15:23:09 -0300

    adicionado recurso de pesquisa de usuarios

--- src/App.js
+++ src/App.js
@@ -1,107 +1,31 @@
-
-import { Button, Input, Col, Row, Form, Select } from 'antd';
-import axios from 'axios';
-import React,{ useState } from 'react';
-const url = "https://api.github.com/search/repositories?q=topic:"
-
-
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom';
+import Home from "./Pages/Home";
+import Favorites from "./Pages/Favorites";
+import {Layout} from 'antd';
+import 'antd/dist/antd.css';
 
 
 function App() {
-	const [List,setList] = useState([]);
-  	const { Option } = Select;
-
-
-	function handleChange(value){
-    	console.log(value)
-  	}
-  
-  async function onSubmit(values){
-	  const response = await axios.get(url+values.search)
-	  const data = await response.data.items
-	  let array = [] 
-	  data.forEach(element => {
-		  const repository = {
-			  project: element.name,
-			  owner : element.owner.login,
-			  avatar : element.owner.avatar_url,
-			  star:false
-		  }
-		  array.push(repository);
-	  });
-
-
-	  setList(array);
-    console.log(List)
-  }
-
-  const Card = (item) => {
-    return (
-      <Card
-        /*actions={[
-          favoritos.find((it) => it.id === item.id) ? (
-            <FaHeart onClick={() => removerFavorito(item)} />
-          ) : (
-            <FaRegHeart onClick={() => favoritar(item)} />
-          ),
-        ]}*/
-        cover={<img alt="example" src={item.avatar} />}
-      >
-        <h3>
-          <a href={item.project} target="_blank">
-            {item.project}
-          </a>
-        </h3>
-        <p>
-          <a href={item.owner} target="_blank">
-            {item.owner}
-          </a>
-        </p>
-      </Card>
-    );
-  };
-
-
-
-
-
   return (
-    <Form onFinish={onSubmit}>
-    	<Col>
-    		<Row>
-    			<Button>Meus Favoritos</Button>
-    		</Row>
-    		<Row>
-      			<Col>
-      				<Select defaultValue="Opções" style={{ width: 120 }} onChange={handleChange}>
-        				<Option value="Repositórios">Repositórios</Option>
-        			</Select>
-      			</Col>
-      			<Col sm={4}>
-      				<Form.Item name="search">
-    					<Input id="search"/>
-    				</Form.Item>
-    			</Col>
-    			<Col>
-    				<Button htmlType="submit">Buscar</Button>
-    			</Col>
-    		</Row>
-			<List
-            grid={{ gutter: 16, column: 6 }}
-            dataSource={List}
-            pagination={{
-              //total: totalCount,
-              pageSize: 30,
-              //onChange: changePage,
-            }}
-            renderItem={(item) => <List.Item>{Card(item)}</List.Item>}
-          />
-			
-    	</Col>
-    </Form>
-    //
-  )
+     <Layout.Content style={{padding:20}}>
+  <Router>
+  <Link to="/">Home</Link> 
+    <Switch>
+    <Route exact path="/">
+      <Home/>
+    </Route>
+    <Route exact path="/favorites">
+    <Favorites/>
+    </Route>
+    </Switch>
+  </Router>
+  </Layout.Content>
+)
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,67 +1,113 @@
-
-import { Button, Input, Col, Row, Form, Select } from 'antd';
+import { Button, Input, Col, Row, Form, Select, List, Card } from 'antd';
 import axios from 'axios';
 import React,{ useState } from 'react';
-const url = "https://api.github.com/search/repositories?q=topic:"
+import { FaHeart, FaRegHeart} from 'react-icons/fa';
+const url = "https://api.github.com/search/"
 
 
 
 
 
-const Home =() => {
-	const [List,setList] = useState([]);
+
+const Home = () =>{
+	const [list,setList] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [favList, setFavList] = useState([]);
+	const [toogle,setToogle] = useState(false);
+	const [cont,setCont] = useState(0);
+	const [option, setOption] = useState('repositories');
   	const { Option } = Select;
 
 
 	function handleChange(value){
-    	console.log(value)
+		setOption(value)
   	}
   
   async function onSubmit(values){
-	  const response = await axios.get(url+values.search)
-	  const data = await response.data.items
+	  //repositories?q=topic:" -> REPOSITORIOS
+	  //search/users?q=MrDuckBr -> users
+	  
+	setLoading(true);
+	  const response = await axios.get(url+ option+'?q='+values.search);
+	  setCont(response.total_count)
 	  let array = [] 
-	  data.forEach(element => {
-		  const repository = {
-			  project: element.name,
-			  owner : element.owner.login,
-			  avatar : element.owner.avatar_url,
-			  star:false
-		  }
-		  array.push(repository);
-	  });
+	  const data = await response.data.items
+	  if(option === 'repositories'){
+		  
+	  		
+	  		data.forEach(element => {
+				const repository = {
+				id: new Date(),
+				project: element.name,
+				owner : element.owner.login,
+				avatar : element.owner.avatar_url,
+				star:false
+				}
+				array.push(repository);
+		 	});
+		}else{
 
+	  		data.forEach(element => {
+				const repository = {
+				id: new Date(),
+				project: element.name,
+				owner : element.type,
+				avatar : element.avatar_url,
+				star:false
+				}
+				array.push(repository);
+		 	});
+
+
+		}
 
 	  setList(array);
-    console.log(List)
+    console.log(list)
+	setLoading(false);
+	console.log('PASSEI AQUI')
   }
 
-  const Card = (item) => {
-    return (
-      <Card
-        /*actions={[
-          favoritos.find((it) => it.id === item.id) ? (
-            <FaHeart onClick={() => removerFavorito(item)} />
-          ) : (
-            <FaRegHeart onClick={() => favoritar(item)} />
-          ),
-        ]}*/
-        cover={<img alt="example" src={item.avatar} />}
-      >
-        <h3>
-          <a href={item.project} target="_blank">
-            {item.project}
-          </a>
-        </h3>
-        <p>
-          <a href={item.owner} target="_blank">
-            {item.owner}
-          </a>
-        </p>
-      </Card>
-    );
-  };
+  function favorites(){
+	let array = []
+	 list.map((itens)=>{
+		if(itens.star === true){
+			array.push(itens)
+		}
+	})
+	setFavList(array)
+}
 
+  function adicionarFav(item){
+	  console.log(item)
+	 const newList = list.map((t)=>{
+		  if(item.avatar === t.avatar){
+			  t.star = true;
+		  }
+		  return t;
+	  })
+	  setList(newList)
+  }
+  
+  function removerFav(item){
+	console.log(item)
+   const newList = list.map((t)=>{
+		if(item.id === t.id){
+			t.star = false;
+		}
+		return t;
+	})
+	setList(newList)
+}
+
+function toogleList(){
+	favorites()
+	if(toogle === false){
+		setToogle(true)
+		
+	}else{
+		setToogle(false)
+	}
+}
 
 
 
@@ -70,12 +116,13 @@ const Home =() => {
     <Form onFinish={onSubmit}>
     	<Col>
     		<Row>
-    			<Button>Meus Favoritos</Button>
+    			<Button onClick={toogleList}>Meus Favoritos</Button>
     		</Row>
     		<Row>
       			<Col>
       				<Select defaultValue="Opções" style={{ width: 120 }} onChange={handleChange}>
-        				<Option value="Repositórios">Repositórios</Option>
+        				<Option value="repositories">Repositórios</Option>
+						<Option value="users">Users</Option>
         			</Select>
       			</Col>
       			<Col sm={4}>
@@ -84,20 +131,51 @@ const Home =() => {
     				</Form.Item>
     			</Col>
     			<Col>
-    				<Button htmlType="submit">Buscar</Button>
+    				<Button loading={loading} htmlType="submit">Buscar</Button>
     			</Col>
     		</Row>
+			{toogle === false ?
 			<List
-            grid={{ gutter: 16, column: 6 }}
-            dataSource={List}
-            pagination={{
-              //total: totalCount,
-              pageSize: 30,
-              //onChange: changePage,
-            }}
-            renderItem={(item) => <List.Item>{Card(item)}</List.Item>}
+			loading={loading}
+            grid={{ gutter: 5, column: 5 }}
+            dataSource={list}
+			pagination={{
+				//total: cont,
+				pageSize: 30,
+				//onChange: changePage,
+			  }}
+            renderItem={(item) => <List.Item>
+				 <Card cover={<img alt="example" src={item.avatar} />}>
+	   				<h3> {item.project}</h3>
+	   				<p>{item.owner}</p>
+					   {item.star === false ? 
+					   <FaRegHeart onClick={() => adicionarFav(item)}/>: <FaHeart onClick={() => removerFav(item)}/>}
+					   
+	 			</Card>
+			</List.Item>}
+          />
+			:
+			<List
+			loading={loading}
+            grid={{ gutter: 5, column: 5 }}
+            dataSource={favList}
+			pagination={{
+				//total: totalCount,
+				pageSize: 30,
+				//onChange: changePage,
+			  }}
+            renderItem={(item) => <List.Item>
+				 <Card cover={<img alt="example" src={item.avatar} />}>
+	   				<h3> {item.project}</h3>
+	   				<p>{item.owner}</p>
+					   {item.star === false ? 
+					   <FaRegHeart onClick={() => adicionarFav(item)}/>: <FaHeart onClick={() => removerFav(item)}/>}
+					   
+	 			</Card>
+			</List.Item>}
           />
 			
+			}
     	</Col>
     </Form>
     //
